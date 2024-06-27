ca1699a303f592345e5031bbab591210d4abf838
Anastasia Granovskaya <iamgranovskaya@gmail.com>
2024-06-27 15:56:52 +0300

    make build

---
+++ index.9a3f3ce4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth/2-t.clientWidth/2+"px",i=e.clientHeight/2-t.clientHeight/2+"px";t.style.top=l,t.style.left=i;
+//# sourceMappingURL=index.9a3f3ce4.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const spiderTop = wall.clientWidth / 2 - spider.clientWidth / 2 + "px";
+const spiderLeft = wall.clientHeight / 2 - spider.clientHeight / 2 + "px";
+spider.style.top = spiderTop;
+spider.style.left = spiderLeft;
+
+//# sourceMappingURL=index.f75de5e1.js.map
